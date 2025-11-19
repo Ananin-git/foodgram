6bb05d9bb22e8a91c0d13012d14de8b98d562054
Mihail Ananin <adocol@yandex.ru>
2025-11-19 22:02:53 +0300

    Ad5

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -59,8 +59,8 @@ AUTH_USER_MODEL = 'recipes.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
